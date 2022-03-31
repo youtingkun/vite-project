e53c638c41ff6a8360a7cb770eea249bb3b30be6
youtingkun <10816@ps.com>
2022-03-31 15:59:13 +0800

    feat: config

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,18 +1,18 @@
 <script setup>
-import { ref } from 'vue'
+import { ref } from "vue";
 
 defineProps({
-  msg: String
-})
+  msg: String,
+});
 
-const count = ref(0)
+const count = ref(0);
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
 
   <p>
-    Recommended IDE setup:
+    Recommended IDE setup:1
     <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
     +
     <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import legacy from "@vitejs/plugin-legacy"; // @vitejs/plugin-legacy插件为传统浏览器提供支持
+import vue from "@vitejs/plugin-vue";
+import path from "path";
 
+const projectRootDir = path.resolve(__dirname);
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+  base: "./",
+  plugins: [
+    vue(),
+    legacy({
+      targets: ["defaults", "not IE 11"],
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": projectRootDir,
+      "@src": path.resolve(projectRootDir, "src"),
+      "@components": path.resolve(projectRootDir, "src/components"),
+      "@assets": path.resolve(projectRootDir, "src/assets"),
+    },
+  },
+});
